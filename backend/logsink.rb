a1cd1d7ed870c409656b66feb2bf4d5dd36e5573
Paul Asmuth <paul@asmuth.com>
2016-12-18 11:21:48 +0100

    UDP log receiver

---
+++ backend/logsink.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+require "socket"
+
+class LogSink
+
+  REMOTE_LOG_PKT_HEADER = "\xff\xff\xff\xffR"
+
+  def initialize
+    @ssock = UDPSocket.new
+    @ssock.bind("0.0.0.0", 3764)
+    @callbacks = []
+  end
+
+  def listen
+    while msg = @ssock.recvfrom(0xffff)
+      payload, raddr = msg
+      payload = payload.force_encoding("utf-8")
+    
+      remote_addrs = [
+        "#{raddr[2]}:#{raddr[1]}",
+        "#{raddr[3]}:#{raddr[1]}"
+      ].uniq
+    
+      unless payload.start_with?(REMOTE_LOG_PKT_HEADER)
+        # FIXME log warning
+        next
+      end
+    
+      logline = payload[REMOTE_LOG_PKT_HEADER.length..-1]
+      while ["\0", "\r", "\n"].include?(logline[-1])
+        logline = logline[0..-2]
+      end
+   
+      @callbacks.each do |cb|
+        cb[logline, remote_addrs]
+      end
+    end
+  end
+
+  def on_logline(&cb)
+    @callbacks << cb
+  end
+
+end
+
+l = LogSink.new
+l.on_logline do |logline, remote_addr|
+  puts logline
+end
+l.listen
