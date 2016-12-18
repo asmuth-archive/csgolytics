92ea687f22ca5919ca127b42070deb3316b1a2ee
Paul Asmuth <paul@asmuth.com>
2016-12-18 15:00:28 +0100

    ...

--- backend/feed_upload.rb
+++ backend/feed_upload.rb
@@ -3,7 +3,9 @@ module CSGOLytics; end
 class CSGOLytics::FeedUpload
 
   def initialize(evql, dispatch)
-    dispatch.subscribe(&:upload_event)
+    dispatch.subscribe do |ev|
+      upload_event(ev)
+    end
   end
 
   def upload_event(ev)
