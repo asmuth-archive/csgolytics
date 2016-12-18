22e025e53b7aeb366100d009fff0f52fcc929c5e
Paul Asmuth <paul@asmuth.com>
2016-12-18 14:51:19 +0100

    csgo_kills schema

--- db/csgo_kills.sql
+++ db/csgo_kills.sql
@@ -1,6 +1,16 @@
 CREATE TABLE csgo_kills (
   time_key uint64,
   event_id string,
+  server_id string,
   time datetime,
+  attacker_name string,
+  attacker_steamid string,
+  attacker_team string,
+  victim_name string,
+  victim_steamid string,
+  victim_team string,
+  weapon string,
+  headshoot bool,
+  penetrated bool,
   PRIMARY KEY (time_key, event_id)
 ) WITH user_defined_partitions = "true";
